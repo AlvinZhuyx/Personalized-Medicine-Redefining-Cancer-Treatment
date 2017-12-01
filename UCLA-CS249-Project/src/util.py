e9add4240ad568b8f1bff32f3cfff3d63d10ee30
Quincy Qu <qqu0127@Quincys-MacBook-Pro.local>
2017-12-01 02:03:04 -0800

    progress made on feature selection part

--- UCLA-CS249-Project/src/util.py
+++ UCLA-CS249-Project/src/util.py
@@ -11,15 +11,18 @@ from gensim.models import Doc2Vec
 from gensim.models.doc2vec import LabeledSentence
 #Gensim is a Python library for *topic modelling*, *document indexing* and *similarity retrieval* with large corpora.
 from gensim import utils as gutils
-'''
-#loading original data
-@return: 
-	all_data, the raw data with 4 fields: [ID, Gene, Variation, Text]
-	train_size, 
-	test_size
+import matplotlib.pyplot as plt
+import itertools
 
-'''
 def loadData():
+    '''
+    #loading original data
+    @return: 
+        all_data, the raw data with 4 fields: [ID, Gene, Variation, Text]
+        train_size, 
+        test_size
+
+    '''
 
 	train_variant = pd.read_csv("../data/training_variants")
 	test_variant = pd.read_csv("../data/stage2_test_variants.csv")
@@ -91,13 +94,50 @@ def cleanup(text):
     text= text.translate(str.maketrans("","", string.punctuation))#去除标点符号
     return text
 
-'''
-@param all_data, the raw data loaded from loadData() function, containing 4 fields: [ID, Gene, Variation, Text]
-@return 
-'''
+
 def data_preprocess(all_data):
+    '''
+    @param all_data, the raw data loaded from loadData() function, containing 4 fields: [ID, Gene, Variation, Text]
+    @return 
+    '''
     allText = all_data['Text'].apply(cleanup) # a list of str
     sentences = constructLabeledSentences(allText)
     return sentences
+
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
 
 
--- UCLA-CS249-Project/src/word_embedding_load.py
+++ UCLA-CS249-Project/src/word_embedding_load.py
@@ -20,16 +20,16 @@ GENE_INPUT_DIM=25
 svd = TruncatedSVD(n_components=GENE_INPUT_DIM, n_iter=25, random_state=12)
 
 
-'''
-Get text_model either from pre-trained models or train it using assigned parameters.
-See also util.Doc2VecParam, util.Doc2VecWrapper()
-
-@param: filename, the path to the 
-@return: text_model for the following
-
-'''
 
 def getTextModel(sentences, param = util.Doc2VecParam(), filename=''):
+    '''
+    Get text_model either from pre-trained models or train it using assigned parameters.
+    See also util.Doc2VecParam, util.Doc2VecWrapper()
+
+    @param: filename, the path to the 
+    @return: text_model for the following
+
+    '''
     if filename == '' or not os.path.isfile(filename):
         print('Creating model...')
         filename = '../model/doc2vec/newModel.d2v'
@@ -49,26 +49,36 @@ def getTextModel(sentences, param = util.Doc2VecParam(), filename=''):
         print('successfully created the Text Model and save it to ' + filename)
         
     else:
-        print('Loading model...')
-        text_model = Doc2Vec.load(filename)
-        print('successfully loaded the textmodel from ' + filename)
+        text_model = loadTextModel(filename)
     return text_model
 
-'''
-Get the clinical text vector representation based on the text_model
+def loadTextModel(filename = ''):
+    if filename == '' or not os.path.isfile(filename):
+        print('Unable to find file: ' + filename)
+        return
+    try:
+        text_model = Doc2Vec.load(filename)
+    except:
+        print('Text Model file exists but unable to load.')
+        return
+    print('Successfully loaded the textmodel from ' + filename)
+    return text_model
 
-@param: 
-    text_model,
-    train_size,
-    test_size,
-    TEXT_INPUT_DIM, set to 200 as default
-@return:
-    text_train_arrays, text vector in training set
-    text_test_arrays, ... in test set
-
-'''
 
 def getTextVec(text_model, train_size, test_size, TEXT_INPUT_DIM = 200):
+    '''
+    Get the clinical text vector representation based on the text_model
+
+    @param: 
+        text_model,
+        train_size,
+        test_size,
+        TEXT_INPUT_DIM, set to 200 as default
+    @return:
+        text_train_arrays, text vector in training set
+        text_test_arrays, ... in test set
+
+    '''
     text_train_arrays = np.zeros((train_size, TEXT_INPUT_DIM))
     text_test_arrays = np.zeros((test_size, TEXT_INPUT_DIM))
     for i in range(train_size):
@@ -79,28 +89,30 @@ def getTextVec(text_model, train_size, test_size, TEXT_INPUT_DIM = 200):
         j += 1
     return text_train_arrays, text_test_arrays
 
-'''
-Get the vector representation for the Gene, the length of the vector is compressed by SVD with default input dimension 25
-@param: 
-    all_data,
-    svd, TruncatedSVD model from sklearn
-@return: truncated_one_hot_gene, gene vector representation
 
-'''
 def getGeneVec(all_data, svd):
+    '''
+    Get the vector representation for the Gene, the length of the vector is compressed by SVD with default input dimension 25
+    @param: 
+        all_data,
+        svd, TruncatedSVD model from sklearn
+    @return: truncated_one_hot_gene, gene vector representation
+
+    '''
     one_hot_gene = pd.get_dummies(all_data['Gene'])
     truncated_one_hot_gene = svd.fit_transform(one_hot_gene.values)
     return truncated_one_hot_gene
 
-'''
-Get the vector representation for the variation type, the length of the vector is compressed by SVD with default input dimension 25
-@param: 
-    all_data,
-    svd, TruncatedSVD model from sklearn
-@return: truncated_one_hot_variation, variation vector representation
 
-'''
 def getVariationVec(all_data, svd):
+    '''
+    Get the vector representation for the variation type, the length of the vector is compressed by SVD with default input dimension 25
+    @param: 
+        all_data,
+        svd, TruncatedSVD model from sklearn
+    @return: truncated_one_hot_variation, variation vector representation
+
+    '''
     one_hot_variation = pd.get_dummies(all_data['Variation'])
     truncated_one_hot_variation = svd.fit_transform(one_hot_variation.values)
     return truncated_one_hot_variation
@@ -108,7 +120,7 @@ def getVariationVec(all_data, svd):
 if __name__ == '__main__':
     TEXT_INPUT_DIM=200
     GENE_INPUT_DIM=25
-    param = Doc2VecParam(1, 5, TEXT_INPUT_DIM, 1e-4, 5, 4, 30, 1)
+    param = util.Doc2VecParam(1, 5, TEXT_INPUT_DIM, 1e-4, 5, 4, 30, 1)
     filename='../model/doc2vec/docEmbeddings_30_load_all.d2v'
 
 
